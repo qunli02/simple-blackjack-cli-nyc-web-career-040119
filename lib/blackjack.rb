3c375303f662b23f1d6b42ddcdbdf66d0c06e4db
Qun Li Huang <qunli02@gmail.com>
2019-03-25 04:37:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,8 @@ end
 
 def invalid_command
   # code invalid_command here
-  puts 
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
